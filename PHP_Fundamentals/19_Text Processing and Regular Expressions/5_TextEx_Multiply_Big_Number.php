0158b66c6bcb35cf780e6c170b8651c7e1844b90
LusiZidarova <75747610+LusiZidarova@users.noreply.github.com>
2020-12-13 10:20:59 +0200

    Add files via upload

---
+++ PHP_Fundamentals/19_Text Processing and Regular Expressions/5_TextEx_Multiply_Big_Number.php
@@ -0,0 +1,28 @@
+<?php
+$num1 = readline();
+$num2 = readline();
+
+echo bcmul($num1,$num2);
+
+/*
+5.Multiply Big Number - 100%
+You are given two lines – the first one can be a really big number (0 to 1050). The second one will be a single digit number (0 to 9). You must display the product of these numbers.
+Note: do not use the function bcmul().
+Examples
+Input			Input	Output		Input	Output
+23
+2
+Output
+46
+Input
+9999
+9
+Output
+89991
+
+Input
+923847238931983192462832102
+4
+Output
+3695388955727932769851328408
+ */
