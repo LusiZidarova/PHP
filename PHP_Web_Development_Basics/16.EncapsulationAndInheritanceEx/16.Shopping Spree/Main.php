379ff94100338d13d39319a0800e97e0193ebf09
LusiZidarova <75747610+LusiZidarova@users.noreply.github.com>
2020-12-15 09:18:56 +0200

    Add files via upload

--- PHP_Web_Development_Basics/16.EncapsulationAndInheritanceEx/16.Shopping Spree/Main.php
+++ PHP_Web_Development_Basics/16.EncapsulationAndInheritanceEx/16.Shopping Spree/Main.php
@@ -54,3 +54,53 @@ foreach ($people as $person){
     echo $person;
 }
 //print_r($people);
+
+/*
+ * Shopping Spree
+Create two classes: class Person and class Product. Each person should have a name, money and a bag of products.
+Each product should have name and cost. Name cannot be an empty string. Money cannot be a negative number.
+Create a program in which each command corresponds to a person buying a product:
+ If the person can afford a product add it to his bag.
+ If a person doesn’t have enough money, print an appropriate message (&quot;[Person name] can&#39;t afford
+[Product name]&quot;).
+On the first two lines you are given all people and all products. Next you will be given all purchases people made
+until END is reached. Print a message when someone makes a purchase. After all purchases print every person in
+the order of appearance and all products that he has bought also in order of appearance. If nothing is bought, print
+the name of the person followed by &quot;Nothing bought&quot;.
+In case of invalid input (negative money exception message: &quot;Money cannot be negative&quot;) or empty name:
+(empty name exception message &quot;Name cannot be empty&quot;) break the program with an appropriate message. See
+the examples below:
+Examples
+Input Output
+Pesho=11;Gosho=4;
+Bread=10;Milk=2;
+Pesho Bread
+Gosho Milk
+Gosho Milk
+Pesho Milk
+END
+
+Output
+Pesho bought Bread
+Gosho bought Milk
+Gosho bought Milk
+Pesho can&#39;t afford Milk
+Pesho - Bread
+Gosho - Milk,Milk
+
+Input
+Mimi=0;
+Kafence=2;
+Mimi Kafence
+END
+
+Output
+Mimi can&#39;t afford Kafence
+Mimi – Nothing bought
+
+Jeko=-3;
+Chushki=1;
+Jeko Chushki
+
+Money cannot be negative
+ */
